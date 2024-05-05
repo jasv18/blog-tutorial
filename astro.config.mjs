452fddf3466dbfeafe97fbb14fc1e7b80fb0f9a0
czeth <44951784+jasv18@users.noreply.github.com>
2024-05-05 00:04:18 -0400

    add astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cblogz.netlify.app/"
+  site: "https://cblogz.netlify.app/",
+  integrations: [preact()]
 });
